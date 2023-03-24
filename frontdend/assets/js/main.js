59ffe0c34cd349da232782eb4b1c6bd76b72d94e
dong959 <binz2028@gmail.com>
2023-03-24 22:35:25 +0700

    Update Search Bar

--- frontdend/assets/js/main.js
+++ frontdend/assets/js/main.js
@@ -89,3 +89,9 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+}
+
+/* filter list search */
